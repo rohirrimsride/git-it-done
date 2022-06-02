7948801d948d2c6c78d31fe3bc766283cfb694dd
Strider <dave.barnes23@gmail.com>
2022-06-02 06:55:28 -0500

    adding issues page

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,4 +1,27 @@
+var repoNameEl = document.querySelector("#repo-name")
 var issueContainerEl = document.querySelector("#issues-container");
+var limitWarningEl = document.querySelector("#limit-warning");
+
+var getRepoIssues = function(repo) {
+    console.log(repo);
+    var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
+    fetch(apiUrl).then(function(response){
+        // request was successful
+        if (response.ok) {
+            response.json().then(function(data){
+                // pass response data to dom function
+                displayIssues(data);
+
+                // check if api has paginated issues
+                if (response.headers.get("Link")) {
+                    displayWarning(repo);
+                }
+            });
+        } else {
+            alert("There was a problem with your request!");
+        }
+    });
+};
 
 var displayIssues = function(issues) {
     if (issues.length === 0) {
@@ -34,21 +57,18 @@ var displayIssues = function(issues) {
     }
 };
 
-var getRepoIssues = function(repo) {
-    console.log(repo);
-    var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
-    fetch(apiUrl).then(function(response){
-        // request was successful
-        if (response.ok) {
-            response.json().then(function(data){
-                // pass response data to dom function
-                displayIssues(data);
-            });
-        } else {
-            alert("There was a problem with your request!");
-        }
-    });
+var displayWarning = function(repo) {
+    // add text to warning container
+    limitWarningEl.textContent = "To see more than 30 issues, vist ";
+
+    var linkEl = document.createElement("a");
+    linkEl.textContent = "GitHub.com";
+    linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+    linkEl.setAttribute("target", "_blank");
+
+    // append to warning container
+    limitWarningEl.appendChild(linkEl);
 };
 
-getRepoIssues("rohirrimsride/conflict-resolution");
+getRepoIssues("angular/angular");
 
